c73817f4134195f8b77e8f5cfad361fb040a0f59
ithingv34 <ithingv34@gmail.com>
2023-06-07 00:24:50 +0900

    redux globalslice for dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+
+const a = createAsyncThunk();
+
 function App() {
+  const b = useSelector();
   return <div className="app">a</div>;
 }
 

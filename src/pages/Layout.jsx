0895deb016b2ddd5677090ee845230625f5758d4
ithingv34 <ithingv34@gmail.com>
2023-06-08 20:15:18 +0900

    Add Layout Page

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,25 @@
-import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import Header from "components/Header";
+import * as React from "react";
+import { useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "components/header/Header";
+import Sidebar from "components/sidebar/Sidebar";
 
 const Layout = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+  const user = useSelector((state) => state.user);
+
   return (
-    <Box height="100%" width="100%">
-      <Box>
-        <Header />
-        <Outlet />
-      </Box>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Header user={user} />
+      <Sidebar
+        open={open}
+        handleDrawerOpen={handleDrawerOpen}
+        handleDrawerClose={handleDrawerClose}
+      />
     </Box>
   );
 };

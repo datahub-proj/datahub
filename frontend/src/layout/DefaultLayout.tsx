0a2216f1c41ecb2d5ca6fa60438d51225f6d384d
ithingv34 <ithingv34@gmail.com>
2023-09-24 08:22:46 +0000

    헤더, BreadCrumb 컴포넌트 추가

--- frontend/src/layout/DefaultLayout.tsx
+++ frontend/src/layout/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import Sidebar from './Sidebar';
+import Header from './Header';
 
 const DefaultLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -11,16 +12,19 @@ const DefaultLayout = () => {
       <div className="flex h-screen overflow-hidden">
         {/* Sidebar */}
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
         {/* Content Area */}
-        <div>Content Area</div>
-        {/*  Header */}
-        <div>Header</div>
-        {/* // Main Content */}
-        <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-            <Outlet />
-          </div>
-        </main>
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {/*  Header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          {/* // Main Content */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              <Outlet />
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
